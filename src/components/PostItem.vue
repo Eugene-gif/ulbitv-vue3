975226a135b1d42a1023f36ad68598438f8b0916
Eugene <zuevevgen93@mail.ru>
2023-06-15 18:44:38 +0300

    Декомпозиция

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,23 +1,32 @@
 <script>
+  import Button from './UI/Button.vue';
+
   export default {
     props: {
       post: {
         type: Object,
         required: false,
       }
-    }
+    }, 
+    components: {Button},
   }
 </script>
 
 <template>
   <div class="post">
-    <div class="post__row">
-      <strong>Название:</strong>
-      <span>{{ post.title || 'Title post' }}</span>
+    <div class="post__wrapper">
+      <div class="post__row">
+        <strong>Название:</strong>
+        <span>{{ post.title || 'Title post' }}</span>
+      </div>
+      <div class="post__row">
+        <strong>Описание:</strong> 
+        <span>{{ post.body || 'Body post' }}</span>
+      </div>
     </div>
-    <div class="post__row">
-      <strong>Описание:</strong> 
-      <span>{{ post.body || 'Body post' }}</span>
+
+    <div class="post__btns">
+      <Button label="Удалить" color="#FF4747" />
     </div>
   </div>
 </template>
@@ -27,6 +36,10 @@
     padding: 15px;
     border: 2px solid teal;
     border-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 15px;
 
     &__row {
       display: flex;
