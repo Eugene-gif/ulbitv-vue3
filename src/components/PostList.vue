a3915bc2404b978fb0b08b5a6b35acfd4551484d
Eugene <zuevevgen93@mail.ru>
2023-06-19 07:54:03 +0300

    Модальное окно + модификаторы v-model

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -18,12 +18,24 @@
 <template>
   <div class="post-list">
     <h3>Список элементов:</h3>
-    <template
-      v-for="post in posts"
-      :key="post.id"
-    >
-      <PostItem :post="post" @remove="$emit('remove',post)" />
+    <template v-if="posts.length > 0">
+      <template
+        v-for="post in posts"
+        :key="post.id"
+      >
+        <PostItem
+          :post="post"
+          @remove="$emit('remove', post)"
+        />
+      </template>
     </template>
+
+    <div
+      class="post-list--empty"
+      v-else
+    >
+      Список элементов пуст
+    </div>
   </div>
 </template>
 
@@ -32,5 +44,14 @@
     display: flex;
     flex-direction: column;
     gap: 15px;
+  }
+
+  .post-list--empty {
+    color: #FF4747;
+    font-size: 28px;
+    font-weight: 600;
+    letter-spacing: 0.3px;
+    line-height: 1.5;
+    margin: 30px auto;
   }
 </style>
---
+++ src/components/UI/Dialog.vue
@@ -0,0 +1,50 @@
+<script>
+  export default {
+    name: "Dialog",
+    props: {
+      show: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    methods: {
+      hideDialog () {
+        this.$emit('update:show', false);
+      }
+    }
+  };
+</script>
+
+<template>
+  <div
+    class="dialog"
+    v-if="show"
+    @click="hideDialog"
+  >
+    <div @click.stop class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .dialog {
+    position: fixed;
+    display: flex;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba($color: #000000, $alpha: 0.4);
+
+    &__content {
+      background-color: #fff;
+      padding: 20px;
+      margin: auto;
+      border-radius: 12px;
+      min-width: 280px;
+      min-height: 50px;
+      max-width: 600px;
+    }
+  }
+</style>
